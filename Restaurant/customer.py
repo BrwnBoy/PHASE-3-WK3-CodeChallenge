799aa824dc9ab0d081a9d705684058e8e5285e58
Brwni <brianmm665@gmial.com>
2023-09-03 10:46:01 +0300

    Initial commit

--- Restaurant/customer.py
+++ Restaurant/customer.py
@@ -6,3 +6,6 @@ class Customer(Base):
     
     # Relationship to Review
     reviews = relationship('Review', back_populates='customer')
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
